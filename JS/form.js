df3f20a55d310b0fbfdeca72aa63368d7118e550
Pabloleabr <pabolo333@gmail.com>
2021-11-30 18:02:44 +0100

    haciendo formulario

---
+++ JS/form.js
@@ -0,0 +1,15 @@
+//JS para la actividad del formulario
+
+const form = document.forms[0];
+
+    form[0].addEventListener('focusout',()=>{
+        if(form[0].checkValidity()){
+            
+        }
+
+    })
+    form[1].addEventListener('focusout',()=>{
+
+    })
+
+
