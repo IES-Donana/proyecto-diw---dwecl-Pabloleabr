5cd5d6fd416d6e7fca737d02111b2ef465851257
Pabloleabr <pabolo333@gmail.com>
2021-12-01 20:36:09 +0100

    formulario a medias

--- JS/form.js
+++ JS/form.js
@@ -2,14 +2,38 @@
 
 const form = document.forms[0];
 
-    form[0].addEventListener('focusout',()=>{
-        if(form[0].checkValidity()){
-            
+//funcion generadora para crear los validar
+function crearValidador(mensajeError){
+    function validar(){
+        if(!this.checkValidity()){
+            if(this.validity.valueMissing){
+                this.setCustomValidity("Debes introducir un valor");
+            }
+            if (this.validity.patternMismatch){
+                this.setCustomValidity(mensajeError);
+                }
+            this.reportValidity();
         }
+        borrarError();
+        //al final por que si iba al principio se me quedaba el mensaje escribiendo
+    }
+    return validar;
+}
 
-    })
-    form[1].addEventListener('focusout',()=>{
 
-    })
+function borrarError(){
+    for(input of form){
+        input.className = "";
+        input.setCustomValidity("");
+    }
+}
+
+var validar = crearValidador("Debe tener menos de 50 caracteres");
+
+    //primer input el del nombre
+form[0].addEventListener('focusout',validar);
+form[1].addEventListener('focusout',validar);
+
+
 
 
